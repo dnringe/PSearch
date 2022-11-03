7a5ac7d183f1392d97e60c69fbcb3fbaeba75eb1
Dattatraya <dnringe@gmail.com>
2022-11-03 12:27:21 +0530

    Adding Navbar.js

--- psearch/src/App.js
+++ psearch/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <Navbar/>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
